2a703513264250a3f9a41f6cbbb05e45cdaf2f7a
Steven van der Baan <steven@vdbaan.net>
2016-11-01 12:03:18 +0000

    Added banner

--- ptrnhndlr.py
+++ ptrnhndlr.py
@@ -3,12 +3,21 @@ from __future__ import print_function
 import argparse
 import string
 
-pattern = ""
+banner = '''
+  _____  _              _    _           _ _      
+ |  __ \| |            | |  | |         | | |     
+ | |__) | |_ _ __ _ __ | |__| |_ __   __| | |_ __ 
+ |  ___/| __| '__| '_ \|  __  | '_ \ / _` | | '__|
+ | |    | |_| |  | | | | |  | | | | | (_| | | |   
+ |_|     \__|_|  |_| |_|_|  |_|_| |_|\__,_|_|_|   
+ '''
+
+pattern = ''
 for c1 in list(string.lowercase):
     for c2 in list(string.uppercase):
         for c3 in list(string.lowercase):
             for c4 in range(0,10):
-                pattern = pattern + "%s%s%s%d"%(c1,c2,c3,c4)
+                pattern = pattern + '%s%s%s%d'%(c1,c2,c3,c4)
 
 def create(size=10):
     '''aAa0aAa1aAa2 ... etc'''
@@ -18,10 +27,11 @@ def create(size=10):
 def locate(ptrn):
     print( pattern.index(ptrn))
 
-if __name__ == "__main__":
+print (banner)
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='ALike the Metasploit pattern_create and pattern_locate')
-    parser.add_argument('-c','--create', metavar="SIZE")
-    parser.add_argument('-l','--locate', metavar="PATTERN")
+    parser.add_argument('-c','--create', metavar='SIZE')
+    parser.add_argument('-l','--locate', metavar='PATTERN')
     args = parser.parse_args()
     if args.create == None and args.locate == None:
         parser.print_help()
